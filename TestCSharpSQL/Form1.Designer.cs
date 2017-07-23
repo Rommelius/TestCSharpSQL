a3d3579b1325403897565ee8bbb05317bd8095e3
Rommel Lapuz <rommel.lapuz@hotmail.com>
2017-07-23 23:19:36 +1000

    Add project files.

---
+++ TestCSharpSQL/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace TestCSharpSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generic ECG";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.LoadDataGridView);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel2);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(916, 566);
+            this.panel1.TabIndex = 5;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(355, 566);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(355, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(561, 566);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 630);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ TestCSharpSQL/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace TestCSharpSQL
+{
+    public partial class Form1 : Form
+    {
+
+        //    public static string myConnectionString = "server=45.32.189.13;uid=rommeladmin;" +
+        //"pwd=sql2303;database=equipment2;convert zero datetime=True";
+        public static string myConnectionString =   "server=127.0.0.1;" +
+                                                    "uid=root;" +
+                                                    "pwd=;" +
+                                                    "database=nvbsystem;" +
+                                                    "convert zero datetime=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            //LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadDataGridView(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            MySqlConnection conn = new MySqlConnection(myConnectionString);
+            conn.Open();
+            try
+            {
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT * FROM tblperformancetest";
+                //MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                //DataSet ds = new DataSet();
+                //adap.Fill(ds);
+                //dataGridView1.DataSource = ds.Tables[2].DefaultView;
+
+                //to get column use reader[x]
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (reader[1].ToString() == b.Text)
+                    {
+                        Label l = new Label();
+                        l.Text = reader[1].ToString();
+                        
+                        flowLayoutPanel1.Controls.Add(l);
+                    }
+                    if (reader[3].ToString() == "1")
+                    {
+                        ComboBox cmbox = new ComboBox();
+                        cmbox.Items.Add("PASS");
+                        cmbox.Items.Add("FAIL");
+                        cmbox.Items.Add("N/A");
+                        flowLayoutPanel2.Controls.Add(cmbox);
+                    }
+                    else if (reader[3].ToString() == "2")
+                    {
+                        TextBox usrinput = new TextBox();
+                        flowLayoutPanel2.Controls.Add(usrinput);
+                    }
+                }
+
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Clone();
+                }
+            }
+
+
+        }
+
+        //private void LoadData()
+        //{
+        //    MySqlConnection conn = new MySqlConnection(myConnectionString);
+        //    conn.Open();
+        //    try
+        //    {
+               
+        //        MySqlCommand cmd = conn.CreateCommand();
+        //        cmd.CommandText = "SELECT * FROM tblperformancetest";
+        //        //MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+        //        //DataSet ds = new DataSet();
+        //        //adap.Fill(ds);
+        //        //dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+        //        //to get column use reader[x]
+
+        //        MySqlDataReader reader = cmd.ExecuteReader();
+
+        //        while (reader.Read())
+        //        {
+        //            if (reader[2].ToString() == "1000022")
+        //            {
+        //                Label b = new Label();
+        //                b.Text = reader[5].ToString();
+        //                flowLayoutPanel1.Controls.Add(b);
+        //            }
+        //        }
+
+                
+        //    }
+        //    catch (MySql.Data.MySqlClient.MySqlException ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //    finally
+        //    {
+        //        if (conn.State == ConnectionState.Open)
+        //        {
+        //            conn.Clone();
+        //        }
+        //    }
+        //}
+
+
+    }
+}
